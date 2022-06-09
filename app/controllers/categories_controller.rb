81c5c222c35989e766bbf675628e53c4ef2c026b
verissimo francis <verissimotoyin50@gmail.com>
2022-06-09 10:01:30 +0100

    Add controllers

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,52 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = current_user.categories.order(created_at: :desc)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @entities = @category.entities.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = set_category
+    @category.destroy
+    redirect_to categories_url
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :image)
+  end
+end
